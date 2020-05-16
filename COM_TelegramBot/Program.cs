31669268c943a1943ca3f217772b1df71858f81c
Ignatella <ignatellasecond@gmail.com>
2020-05-16 15:55:03 +0200

    Create SearchInTextBox method
    Now the app can find words in text boxes. Add NuGet packege
    Microsoft.Office.Interop.Word

--- COM_TelegramBot/Program.cs
+++ COM_TelegramBot/Program.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace COM_TelegramBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static Word.Application wordApp;
+        public static Document wordDoc;
+        public static void Main(string[] args)
         {
+            try
+            {
+                wordApp = new Word.Application();
+
+                wordDoc = wordApp.Documents.OpenNoRepairDialog(FileName: @"C:\Users\Mi\Desktop\a.doc", ReadOnly: false);
+
+                string strToSearhFor = "protest";
+
+                SearchInTextBox(strToSearhFor);
+
+
+                wordApp.Visible = true;
+                Console.ReadLine();
+            }
+            finally
+            {
+                wordDoc.Close(false);
+                wordApp.Quit(false);
+            }
+        }
+
+        public static void SearchInTextBox(string wordToSearchFor)
+        {
+            foreach (Shape shape in wordApp.ActiveDocument.Shapes)
+            {
+                if (shape.TextFrame.HasText == -1 && shape.TextFrame.TextRange.Text.ToLower().Contains(wordToSearchFor))
+                {
+                    Range rang = shape.TextFrame.TextRange;
+                    int wordPosition = shape.TextFrame.TextRange.Text.IndexOf(wordToSearchFor);
+                    rang.Select();
+                    wordApp.Selection.HomeKey(WdUnits.wdLine, WdMovementType.wdMove);
+                    wordApp.Selection.MoveRight(WdUnits.wdCharacter, wordPosition, WdMovementType.wdMove);
+                    wordApp.Selection.MoveRight(WdUnits.wdWord, 1, WdMovementType.wdExtend);
+                    wordApp.Selection.Range.HighlightColorIndex = WdColorIndex.wdYellow;
+                }
+            }
         }
     }
 }
