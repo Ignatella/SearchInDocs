9e68e79074e4c2feefc4eb5ba69700c89b022750
Ignatella <ignatellasecond@gmail.com>
2020-05-17 14:05:36 +0200

    Add Extension.cs with Foreach extension method

---
+++ COM_TelegramBot/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM_TelegramBot
+{
+    public static class Extensions
+    {
+        public static void Foreach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
